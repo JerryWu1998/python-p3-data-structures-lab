d681cb3884498ac21047463d05bcd8f80545bf38
Yu Wu <yw3748@columbia.edu>
2023-07-05 17:59:19 -0400

    lab

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -16,23 +16,45 @@ spicy_foods = [
     },
 ]
 
+
 def get_names(spicy_foods):
-    pass
+    return [food["name"] for food in spicy_foods]
+
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [food for food in spicy_foods if food["heat_level"] > 5]
+
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        print(
+            f"{food['name']} ({food['cuisine']}) | Heat Level: {food['heat_level'] * '🌶'}"
+        )
+
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
+
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        if food["heat_level"] > 5:
+            print(
+                f"{food['name']} ({food['cuisine']}) | Heat Level: {food['heat_level'] * '🌶'}"
+            )
+
 
 def get_average_heat_level(spicy_foods):
-    pass
+    count = 0
+    sum = 0
+    for food in spicy_foods:
+        sum += food["heat_level"]
+        count += 1
+    return sum / count
+
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
